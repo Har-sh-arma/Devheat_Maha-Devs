715086e5c5b0f12c01cee2ed27a48681f0882ec2
Har-sh-arma <harinathsharma303@gmail.com>
2022-11-19 23:29:07 +0530

    merge conflict fixing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Lspage from './components/Login_Signup/Lspage';
 import Spage from './components/Login_Signup/Spage';
 import Main from './components/Mainpg/Main'
 import Otp from "./components/Login_Signup/Otp";
-import MapsMain from "./components/Location/MapsMain";
 import Description from "./components/Mainpg/Description";
 import Details from "./components/Cropdetails/Details";
 
@@ -19,7 +18,6 @@ function App() {
           <Route path='/Spage' element={<Spage />} />
           <Route path='/Main' element={<Main />} />
           <Route path="/Otp" element={<Otp />} />
-          <Route path="/MapsMain" element={<MapsMain />} />
           <Route path='/Description' element={<Description />} />
           <Route path='/Details' element={<Details />} />
         </Routes>
